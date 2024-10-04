c2138e7519b25d9aa66eca4e817c74262db5c263
Dmitry Torba <dmitrytorba@gmail.com>
2024-10-04 10:04:29 -0700

    feat(maps) render map

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+async function initGoogleMap() {
+  const { Map3DElement } = await google.maps.importLibrary("maps3d") as any;
 
+  const map = new Map3DElement({
+    center: { lat: 37.36353, lng: -121.9286, altitude: 0 },
+    tilt: 67.5,
+    range: 1000
+  });
+
+  document.body.append(map);
+
+  // map.flyCameraTo({
+  //   endCamera: {
+  //     center: { lat: 37.6191, lng: -122.3816 },
+  //     tilt: 67.5,
+  //     range: 1000
+  //   },
+  //   durationMillis: 5000
+  // });
+}
+initGoogleMap();
+
+function App() {
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
